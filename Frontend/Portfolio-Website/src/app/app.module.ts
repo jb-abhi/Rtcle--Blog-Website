8d59428cbfb5a074cdb3dd4fae70ebec96a1521f
jb-abhi <jbabhi2@gmail.com>
2022-06-30 15:20:45 +0530

    Landing-page-complete

--- Frontend/Portfolio-Website/src/app/app.module.ts
+++ Frontend/Portfolio-Website/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, LandingPageComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- Frontend/Portfolio-Website/src/app/landing-page/landing-page.component.ts
+++ Frontend/Portfolio-Website/src/app/landing-page/landing-page.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss']
+  styleUrls: ['./landing-page.component.scss'],
 })
 export class LandingPageComponent implements OnInit {
+  faArrowRight = faArrowRightLong;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
